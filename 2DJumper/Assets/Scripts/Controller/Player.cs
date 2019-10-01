d1745c3d218f2cf66a7a4b662571e074402e59af
Matt-Rader <raderm@umich.edu>
2019-10-01 00:23:41 -0400

    Started work on writing physics for the character controller

---
+++ 2DJumper/Assets/Scripts/Controller/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour
+{
+	[SerializeField] private float jumpHeight = 4;
+	[SerializeField] private float timeToJumpApex = 0.4f;
+	[SerializeField] private float moveSpeed = 6;
+	[SerializeField] private float accelerationTimeAirborne = 0.2f;
+	[SerializeField] private float accelerationTimeGrounded = 0.1f;
+
+
+	float jumpVelocity;
+	float gravity;
+	Vector3 velocity;
+	float velocityXSmoothing;
+
+	Controller2D controller;
+
+    void Start()
+    {
+		controller = GetComponent<Controller2D>();
+
+		gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+		print("Gravity: " + gravity + " JumpVelocity: " + jumpVelocity);
+    }
+
+    void Update()
+    {
+		if (controller.collInfo.above || controller.collInfo.below)
+		{
+			velocity.y = 0;
+		}
+
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+		if (Input.GetButtonDown("Jump") && controller.collInfo.below)
+		{
+			velocity.y = jumpVelocity;
+		}
+
+		float targetVelocityX = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, 
+			(controller.collInfo.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move(velocity * Time.deltaTime);
+    }
+}
