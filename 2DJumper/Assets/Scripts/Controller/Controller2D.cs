ddcc22b1531877af57313e6e79c42f19a2b50180
Matt-Rader <raderm@umich.edu>
2019-10-01 23:56:31 -0400

    Little more work on the character controller
    Following along with Sebastian Langue's character controller tutorial, made it to episode 6

--- 2DJumper/Assets/Scripts/Controller/Controller2D.cs
+++ 2DJumper/Assets/Scripts/Controller/Controller2D.cs
@@ -19,6 +19,9 @@ public class Controller2D : MonoBehaviour
 	float horizontalRaySpacing;
 	float verticalRaySpacing;
 
+	float maxClimbAngle = 50f;
+	float maxDescendAngle = 75f; 
+
 	BoxCollider2D collider;
 	RaycastOrigins raycastOrigins;
 	public CollisionInfo collInfo;
@@ -31,9 +34,12 @@ public class Controller2D : MonoBehaviour
 
 	public void Move(Vector3 velocity)
 	{
-		collInfo.Reset();
-
 		UpdateRaycastOrigins();
+		collInfo.Reset();
+		collInfo.velocityOld = velocity;
+
+		if (velocity.y < 0)
+			DescendSlope(ref velocity);
 
 		if (velocity.x != 0) 
 			HorizontalCollisions(ref velocity);
@@ -59,11 +65,41 @@ public class Controller2D : MonoBehaviour
 
 			if (hit)
 			{
-				velocity.x = (hit.distance - skinWidth) * directionX;
-				rayLength = hit.distance;
+				float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
 
-				collInfo.left = directionX == -1;
-				collInfo.right = directionX == 1;
+				if (i == 0 && slopeAngle <= maxClimbAngle)
+				{
+					if (collInfo.descendingSlope)
+					{
+						collInfo.descendingSlope = false;
+						velocity = collInfo.velocityOld;
+					}
+
+					float distanceToSlopeStart = 0;
+					if (slopeAngle != collInfo.slopeAngleOld)
+					{
+						distanceToSlopeStart = hit.distance - skinWidth;
+						velocity.x -= distanceToSlopeStart * directionX;
+					}
+
+					ClimbSlope(ref velocity, slopeAngle);
+
+					velocity.x += distanceToSlopeStart * directionX;
+				}
+
+				if (!collInfo.climbingSlope || slopeAngle > maxClimbAngle)
+				{
+					velocity.x = Mathf.Min(Mathf.Abs(velocity.x), (hit.distance - skinWidth)) * directionX;
+					rayLength = Mathf.Min(Mathf.Abs(velocity.x) + skinWidth, hit.distance);
+
+					if (collInfo.climbingSlope)
+					{
+						velocity.y = Mathf.Tan(collInfo.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x);
+					}
+
+					collInfo.left = directionX == -1;
+					collInfo.right = directionX == 1;
+				}
 			}
 		}
 	}
@@ -86,8 +122,79 @@ public class Controller2D : MonoBehaviour
 				velocity.y = (hit.distance - skinWidth) * directionY;
 				rayLength = hit.distance;
 
+				if (collInfo.climbingSlope)
+				{
+					velocity.x = velocity.y / Mathf.Tan(collInfo.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign(velocity.x);
+				}
+
 				collInfo.above = directionY == 1;
 				collInfo.below = directionY == -1;
+			}
+		}
+
+		if (collInfo.climbingSlope)
+		{
+			float directionX = Mathf.Sign(velocity.x);
+			rayLength = Mathf.Abs(velocity.x) + skinWidth;
+			Vector2 rayOrigin = ((directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight) + Vector2.up * velocity.y;
+			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+			if (hit)
+			{
+				float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+				if (slopeAngle != collInfo.slopeAngle)
+				{
+					velocity.x = (hit.distance - skinWidth) * directionX;
+					collInfo.slopeAngle = slopeAngle;
+				}
+			}
+		}
+	}
+
+	void ClimbSlope(ref Vector3 velocity, float slopeAngle)
+	{
+		float moveDistance = Mathf.Abs(velocity.x);
+		float climbVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+
+		if (velocity.y <= climbVelocityY)
+		{
+			velocity.y = climbVelocityY;
+			velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+			collInfo.below = true;
+			collInfo.climbingSlope = true;
+			collInfo.slopeAngle = slopeAngle;
+		}
+	}
+
+	void DescendSlope(ref Vector3 velocity)
+	{
+		float directionX = Mathf.Sign(velocity.x);
+		Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomRight : raycastOrigins.bottomLeft;
+		RaycastHit2D hit = Physics2D.Raycast(rayOrigin, -Vector2.up, Mathf.Infinity, collisionMask);
+
+		if (hit)
+		{
+			float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+			if (slopeAngle != 0 && slopeAngle <= maxDescendAngle)
+			{
+				if (Mathf.Sign(hit.normal.x) == directionX)
+				{
+					if (hit.distance - skinWidth <= Mathf.Tan(slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x))
+					{
+						float moveDistance = Mathf.Abs(velocity.x);
+
+						float descendVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+						velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+						velocity.y -= descendVelocityY;
+
+						collInfo.slopeAngle = slopeAngle;
+						collInfo.descendingSlope = true;
+						collInfo.below = true;
+					}
+				}
+
 			}
 		}
 	}
@@ -126,10 +233,17 @@ public class Controller2D : MonoBehaviour
 		public bool above, below;
 		public bool left, right;
 
+		public bool climbingSlope, descendingSlope;
+		public float slopeAngle, slopeAngleOld;
+		public Vector3 velocityOld;
+
 		public void Reset()
 		{
 			above = below = false;
 			left = right = false;
+			climbingSlope = descendingSlope = false;
+			slopeAngleOld = slopeAngle;
+			slopeAngle = 0;
 		}
 	}
 }
--- 2DJumper/Assets/Scripts/Controller/Player.cs
+++ 2DJumper/Assets/Scripts/Controller/Player.cs
@@ -25,7 +25,6 @@ public class Player : MonoBehaviour
 
 		gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
 		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-		print("Gravity: " + gravity + " JumpVelocity: " + jumpVelocity);
     }
 
     void Update()
